6f5e6128be94f76670ce0b0e53ee43bf3f20c8cf
Colin Greybosh <greybosh@mit.edu>
2021-08-09 16:54:58 -0700

    Refactored to use dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import winston from 'winston';
 
+dotenv.config();
+
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.SERVER_PORT; // default port to listen
+
+const logDirectory = process.env.LOG_DIRECTORY;
 
 const logger: winston.Logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports: [
         // - Write all logs with level `error` and below to `error.log`
-        new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: `${logDirectory}/error.log`, level: 'error' }),
         // - Write all logs with level `info` and below to `combined.log`
-        new winston.transports.File({ filename: './logs/combined.log' }),
+        new winston.transports.File({ filename: `${logDirectory}/combined.log` }),
     ],
 });
 
