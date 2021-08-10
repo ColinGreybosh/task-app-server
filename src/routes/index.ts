62b163c251e95c3262d0cc7d7cbb396e5a3780df
Colin Greybosh <greybosh@mit.edu>
2021-08-10 15:13:37 -0700

    Added routing boilerplate code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 
+import * as login from './login';
+import * as users from './users';
+import * as tasks from './tasks';
+import * as sessions from './sessions';
+
 const router: express.Router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Hello, world!');
 });
+
+router.use(login.router);
+router.use(users.router);
+router.use(tasks.router);
+router.use(sessions.router);
 
 export default router;
